165a294667b532f88953cae112f69a4eb49e43f2
Kessler Yaron <yaron.kessler42@gmail.com>
2017-06-07 15:45:47 +0300

    description

--- JavaBasic/JavaBasic.java
+++ JavaBasic/JavaBasic.java
@@ -2,22 +2,62 @@
 //class: every line of code in java need to be in a "class".
 public class JavaBasic {
 	
-	//static - no need for instance of the class
-	public static int intCount = 0;
+	/*
+	 * variable declaration
+	 */
 	
-	//ini - integer
-	private int age;
-	
-	// String sequence of characters.
-	private String StDescribe;
-	
+	public static int intCount = 0;	//static - no need for instance of the class
+	private int age;	//ini - integer
+	private String StDescribe;	// String sequence of characters.
 	private boolean flage = false;
-	
 	private char grade = 'A';
+	
+	//constraction
+	public JavaBasic() {
+		//super();
+		// TODO Auto-generated constructor stub
+	}
 
+	/**
+	 * Methods examples
+	 */
+//	method with NO parameters
+	public void methodCalling()	{
+		System.out.println("this is a method without parameters");
+	}
+
+//	method with one parameters
+	public void methodCalling(int parameterOne){
+		System.out.println("method with a parameters , parameterOne >> " +  parameterOne);
+	}
+	
+//	method + 2 parameters
+	public void methodCalling(int parameterOne, int parameterTwo){
+		System.out.println("method with a parameters , parameterOne >> " +  parameterOne + " , "+ parameterTwo);
+	}
+	
+
+	public void methodPrePostFix(){
+		//++increment++
+		//--decrement--		
+		//
+		//prefix
+		int a = 2;
+		int b = ++a; // a=3, b=3
+		System.out.println("prefix >> a = 2, b = ++a ====> a== " + a + " b== " + b);
+		//
+		//postfix
+		int c = 3;
+		int d = c--; // d=3 c=2
+		System.out.println("postfix >> c = 3, d = c-- ====> c== " + c + " d == " + d);
+
+	}
+	
+	
+	
 	
 	/*
-	 * Getter / Setter
+	 * Getter , Setter
 	 */
 	public static int getIntCount() {
 		return intCount;
@@ -38,28 +78,18 @@ public class JavaBasic {
 	public String getStDescribe() {
 		return StDescribe;
 	}
-
+	
 	public void setStDescribe(String stDescribe) {
 		StDescribe = stDescribe;
 	}
-	
+	/**
+	 * /Getter , /Setter
+	 */
 	
 	
 	//main method (function)
 	public static void main(String[] args) {
 
-		//++increment++
-		//--decrement--		
-		//
-		//prefix
-		int a = 2;
-		int b = ++a; // a=3, b=3
-		System.out.println("prefix >> a = 2, b = ++a ====> a== " + a + " b== " + b);
-		//
-		//postfix
-		int c = 3;
-		int d = c--; // d=3 c=2
-		System.out.println("postfix >> c = 3, d = c-- ====> c== " + c + " d == " + d);
 
 			
 		JavaBasic jb = new JavaBasic();
